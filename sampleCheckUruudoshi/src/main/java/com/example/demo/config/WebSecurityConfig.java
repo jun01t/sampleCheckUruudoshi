bb9bc47b96b8dad65cdcefb22603140107615f33
jun01t <tmdjnch0901@gmail.com>
2022-04-10 13:20:03 +0900

    ログイン失敗時のデザイン修正。

--- sampleCheckUruudoshi/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ sampleCheckUruudoshi/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/images/**", "/css/**", "/javascript/**", "assets/**");
+		web.ignoring().antMatchers("/images/**", "/css/**", "/javascript/**", "/assets/**");
 	}
 
 	/**
