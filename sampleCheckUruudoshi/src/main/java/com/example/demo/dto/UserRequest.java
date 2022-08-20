6a7fcb709147c932cdf0a94fe6a0a731417f2850
junichi <tmdjnch0901@gmail.com>
2022-08-20 14:42:16 +0900

    macOSに対応。

--- sampleCheckUruudoshi/src/main/java/com/example/demo/dto/UserRequest.java
+++ sampleCheckUruudoshi/src/main/java/com/example/demo/dto/UserRequest.java
@@ -3,9 +3,12 @@ package com.example.demo.dto;
 import java.io.Serializable;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * ユーザー情報 リクエストデータ
@@ -36,5 +39,41 @@ public class UserRequest implements Serializable {
 	@NotEmpty(message = "ロールを選択してください")
 	@Size(max = 2)
 	private String role;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	/**
+	 * ユーザーID
+	 */
+	@NotNull
+	private Long id;
+
+	public void setId(Long userId) {
+		this.id = userId;
+
+	}
+
+	public void setName(String userName) {
+		this.name = userName;
+
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
--- sampleCheckUruudoshi/src/main/java/com/example/demo/dto/UserUpdateRequest.java
+++ sampleCheckUruudoshi/src/main/java/com/example/demo/dto/UserUpdateRequest.java
@@ -24,4 +24,13 @@ public class UserUpdateRequest extends UserRequest implements Serializable {
 	 */
 	@NotNull
 	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long userId) {
+		this.id = userId;
+
+	}
 }
